b24adbc4f2a69d551992f4812dfcdbfc5a469829
boniface <boniface@kabaso.com>
2018-08-21 06:57:03 +0200

    Added DOcker File

--- src/main/java/za/ac/cput/javanosqltest/repository/cassandra/CassandraRepository.java
+++ src/main/java/za/ac/cput/javanosqltest/repository/cassandra/CassandraRepository.java
@@ -1,14 +1,22 @@
 package za.ac.cput.javanosqltest.repository.cassandra;
 
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
 import za.ac.cput.javanosqltest.domain.Person;
 import za.ac.cput.javanosqltest.repository.Repository;
 
 import java.util.List;
 
 public class CassandraRepository implements Repository {
+    final Cluster cluster = Cluster
+            .builder()
+            .addContactPoint("127.0.0.1")
+            .build();
+    final Session session = cluster.connect("users");
     @Override
     public Person create(Person person) {
+        session.execute("INSERT INTO persons (id, name) VALUES ("+ person.getId()+ ","+person.getName()+")");
         return null;
     }
 
--- src/main/java/za/ac/cput/javanosqltest/services/dgraph/DgraphServiceImpl.java
+++ src/main/java/za/ac/cput/javanosqltest/services/dgraph/DgraphServiceImpl.java
@@ -1,4 +1,26 @@
 package za.ac.cput.javanosqltest.services.dgraph;
 
-public class DgraphServiceImpl {
+import za.ac.cput.javanosqltest.domain.Result;
+import za.ac.cput.javanosqltest.services.Service;
+
+public class DgraphServiceImpl implements Service {
+    @Override
+    public Result create(Long number) {
+        return null;
+    }
+
+    @Override
+    public Result read() {
+        return null;
+    }
+
+    @Override
+    public Result update() {
+        return null;
+    }
+
+    @Override
+    public Result delete() {
+        return null;
+    }
 }
