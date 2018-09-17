4d7cc1185309bc14bc2b32bc7a7c8bbad6266a8e
boniface <boniface@kabaso.com>
2018-09-17 15:56:41 +0200

    Cassandra working 100%

--- src/main/java/za/ac/cput/javanosqltest/App.java
+++ src/main/java/za/ac/cput/javanosqltest/App.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import za.ac.cput.javanosqltest.domain.Result;
+import za.ac.cput.javanosqltest.services.cassandra.CassandraServiceImpl;
 import za.ac.cput.javanosqltest.services.mongo.Impl.MongoServiceImpl;
 import za.ac.cput.javanosqltest.services.redis.Impl.RedisServiceImpl;
 import za.ac.cput.javanosqltest.services.Service;
@@ -71,6 +72,32 @@ public class App {
     @RequestMapping(value="/mongo/delete", method=RequestMethod.GET)
     public Result mongoDelete() {
         service = new MongoServiceImpl();
+        return service.delete();
+    }
+
+    // Cassandra CRUD
+
+    @RequestMapping(value="/cassandra/create/{number}", method=RequestMethod.GET)
+    public Result cassandraCreate(@PathVariable Long number) {
+        service = new CassandraServiceImpl();
+        return service.create(number);
+    }
+
+    @RequestMapping(value="/cassandra/read", method=RequestMethod.GET)
+    public Result cassandraRead() {
+        service = new CassandraServiceImpl();
+        return service.read();
+    }
+
+    @RequestMapping(value="/cassandra/update", method=RequestMethod.GET)
+    public Result cassandraUpdate() {
+        service = new CassandraServiceImpl();
+        return service.update();
+    }
+
+    @RequestMapping(value="/cassandra/delete", method=RequestMethod.GET)
+    public Result cassandraDelete() {
+        service = new CassandraServiceImpl();
         return service.delete();
     }
 
